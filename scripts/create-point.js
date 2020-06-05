9e9a21829eb99728d6d30cca97cc0852506d8df2
Marciele Adivíncula <marcieleadivincula@gmail.com>
2020-06-05 20:24:11 -0300

    Terceiro dia

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,5 @@
 function populateUFs() {
-  const ufSelect = document.querySelector("select[name=uf]"); 
+  const ufSelect = document.querySelector("select[name=uf]");
 
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then((resp) => resp.json())
@@ -23,6 +23,9 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+  citySelect.innerHTML = `<option>Selecione a cidade</option>`;
+  citySelect.disabled = true;
+
   fetch(url)
     .then((resp) => resp.json())
     .then((cities) => {
@@ -36,7 +39,56 @@ function getCities(event) {
 
 document.querySelector("[name=uf]").addEventListener("change", getCities);
 
+//Itens de coleta
+//pefar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
 
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+let selectedItems = [2,3];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // add or remove uma class com JS
+  //Com o toggle ele verifica o elemento, se existe ele remove, se não existir ele adiciona
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  //verificar se existem items selecionados, se sim pegar os itens selecionados
+  const alreadySelected = selectedItems.findIndex(item => {
+    return item === itemId; //isso será true ou false
+  });
+
+  //se já estiver selecionado,
+  //alreadySelected >= 0 ou alreadySelected != -1
+  if (alreadySelected >= 0) {
+    //tirar da seleção
+    const filteredItems = selectedItems.filter(item => {
+        //quando o return for false é o momento que deverá ser removido do array
+        return item !== itemId; //false
+    });
+
+    console.log(filteredItems);
+  }
+  //se não estiver selecionado, adicionar à seleção
+
+  //atualizar o campo escondido com os itens selecionados
+}
+
+//Dessa forma está detalhada, mas em cima está simplificada
+// const alreadySelected = selectedItems.findIndex(function (item) {
+//     const itemFound = item === itemId;
+//     return itemFound;
+// });
+
+// const filteredItems = selectedItems.filter((item) => {
+//   const itemsIsDifferent = item !== itemId; //false
+//   return itemsIsDifferent;
+// });
 
 //fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(function(res) { return res.json()}).then(function(data){console.log(data)})
 
