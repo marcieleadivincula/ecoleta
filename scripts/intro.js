72e28485eadea2cb512e09b52e75ba1533b7eec4
Marciele Adivíncula <marcieleadivincula@gmail.com>
2020-06-05 13:25:46 -0300

    Segundo dia

---
+++ scripts/intro.js
@@ -0,0 +1,71 @@
+//document.write("Hello");
+//var myvar = "Hello";
+//const myConst = "World";
+//
+//document.write(myConst + myvar);
+
+//number
+//const n1 = 1;
+//const n2 = 12;
+//
+//document.write(n1 + n2);
+
+//boolean
+//const bTrue = true;
+//const bFalse = false;
+//
+//document.write(bTrue);
+//document.write(bFalse);
+
+//object possuem propriedades e funcionalidade
+//const pessoa = {
+//  altura: "1.80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr() {
+//    document.write("Executar uma grande lógica de correr");
+//    //return "run forest";
+//  }
+//};
+
+//document.write(pessoa.altura);
+//document.write(pessoa.idade);
+//document.write(pessoa.solteiro);
+//document.write(pessoa.correr());
+
+//pessoa.correr();
+
+//Array - Vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: 'Maria', idade: 20, sexo: 'F', peso: 53.5, casada: true} ];
+
+//document.write(colecaoDeBolinhas[0]);
+//document.write(colecaoDeBolinhas[3].name);
+
+//function - funcionalidades - atalhos - reuso de código
+//registrar função
+//function sayMyName(name){
+//  document.write(name);
+//}
+
+//executar
+//sayMyName("Marciele");
+//sayMyName("Júlia");
+//sayMyName("Igor");
+//sayMyName("Douglas");
+
+
+//Conditionals 
+//const notaFinal = 7;
+//
+//if (notaFinal < 5) {
+//  document.write("Reprovado");
+//} else {
+//  document.write("Aprovado");
+//}
+
+//loop repetições
+//i++ mesmo que dizer i = i + 1
+//for (i = 0; i < 10; i++) {
+//  document.write(`<p> ${i} </p>`);
+//}
+
